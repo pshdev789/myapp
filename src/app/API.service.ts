b13f96052220c7a9e7232434207a5919392d4b75
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-10-17 19:20:15 +0530

    Initial Changes

---
+++ src/app/API.service.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+import { Injectable } from "@angular/core";
+import API, { graphqlOperation, GraphQLResult } from "@aws-amplify/api-graphql";
+import { Observable } from "zen-observable-ts";
+
+@Injectable({
+  providedIn: "root"
+})
+export class APIService {}
